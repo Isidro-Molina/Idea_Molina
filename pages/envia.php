807e1969df23fecf4927db9c127babce8a3102e0
Isidro Molina <isidromolina260@gmail.com>
2022-06-22 18:44:01 -0300

    mejoras al php

--- pages/envia.php
+++ pages/envia.php
@@ -11,10 +11,10 @@ if (!$_POST){
 }else{
 
         $cuerpo = "Nombre: " . $_POST["nombre"] . "\r\n";
-        $cuerpo = "Apellido: " . $_POST["apellido"] . "\r\n";
-        $cuerpo .= "Email: " . $_POST ["email"] . "\r\n";
-        $cuerpo .= "Motivo " . $_POST ["motivo"] . "\r\n";
-        $cuerpo .= "Contanos... " . $_POST ["textazo"] . "\r\n";
+        $cuerpo .= "Apellido: " . $_POST["apellido"] . "\r\n";
+        $cuerpo .= "Email: " . $_POST["email"] . "\r\n";
+        $cuerpo .= "Motivo: " . $_POST["motivo"] . "\r\n";
+        $cuerpo .= "Contanos... " . $_POST["textazo"] . "\r\n";
 
         $headers = "MIME-Version: 1.0";
         $headers .= "Content-type: text/plain: charset-utf-8\n";
@@ -26,7 +26,7 @@ if (!$_POST){
 
         mail($destinatario, $asunto, $cuerpo, $headers);
 
-        include 'noticias.html';
+        include 'gracias.html';
 
 }
 
